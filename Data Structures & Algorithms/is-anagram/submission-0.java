f8bc4662e5e4e785d786a228c964d950dbb9adf1
Omkarreddy2005 <147788468+Omkarreddy2005@users.noreply.github.com>
2026-06-20 08:11:27 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        if(n!=m) return false;
+        char ch[]=s.toCharArray();
+        char ch1[]=t.toCharArray();
+        Arrays.sort(ch);
+        Arrays.sort(ch1);
+        String s1=new String(ch);
+        String s2=new String(ch1);
+        if(s1.equals(s2)) return true;
+        return false;
+    }
+}
